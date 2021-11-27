0b2187b6bf58b66f0be569dd6c65671375308ff1
liguobao <codelover@qq.com>
2021-11-27 17:03:15 +0800

    458.poorPigs

--- daily-code/kmp.py
+++ daily-code/kmp.py
@@ -1,3 +1,6 @@
+from typing import Text
+
+
 class Solution():
     def __init__(self) -> None:
         pass
@@ -6,18 +9,36 @@ class Solution():
         self.pat_len = len(pat)
         self.pat = pat
         self.dp = [[0 for i in range(255)] for j in range(self.pat_len)]
-        self.dp[0][ord(pat[0])] = 1
+        first_c = ord(pat[0])
+        self.dp[0][first_c] = 1
         X = 0
-        for j in range(self.pat_len):
+        for j in range(1, self.pat_len):
+            # j pat字符串第j个元素
+            pat_c = pat[j]
+            pat_c_val = ord(pat_c)
             for c in range(255):
-                pat_c = pat[j]
-                if ord(pat_c) == c:
+                if pat_c_val == c:
                     self.dp[j][c] = j + 1
                 else:
                     self.dp[j][c] = self.dp[X][c]
-            pat_c = pat[j]
-            X = self.dp[X][ord(pat_c)]
-        print(self.dp)
+            X = self.dp[X][pat_c_val]
+    
+    def search(self, txt):
+        M = self.pat_len
+        N = len(txt)
+        # pat 的初始态为 0
+        j = 0
+        for i in range(N):
+            # 当前是状态 j，遇到字符 txt[i]，
+            # pat 应该转移到哪个状态？
+            txt_c = txt[i]
+            j = self.dp[j][ord(txt_c)]
+            # 如果达到终止态，返回匹配开头的索引
+            if (j == M):
+                return i - M + 1
+        #没到达终止态，匹配失败
+        return -1
+
 
     def search_by_pat(self, pat, text):
         pat_len = len(pat)
@@ -33,7 +54,13 @@ class Solution():
 
 
 solution = Solution()
-solution.init_dp("ababc")
+solution.init_dp("ABABC")
+result = solution.search("AAA")
+print(result)
+result = solution.search("AAABABCCCC")
+print(result)
+result = solution.search("CCCCAABABCCCC")
+print(result)
 result = solution.search_by_pat("abc", "abaababcc")
 print(result)
 result = solution.search_by_pat("cba", "aaaababcc")
