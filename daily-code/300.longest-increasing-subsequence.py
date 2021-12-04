882f1d7b309f3e6d486288cd0e3fee485885e3fa
liguobao <codelover@qq.com>
2021-12-04 13:05:37 +0800

    add 300

---
+++ daily-code/300.longest-increasing-subsequence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        pass
+
+solution =Solution()
+nums = [-2,0,1,3]
+result = solution.lengthOfLIS(nums)
+print(f"{nums},result:{result}")
