cfdca21603289cc6ddbe1eb74dd54e80bc164398
liguobao <codelover@qq.com>
2022-02-02 12:21:58 +0800

    cd /Users/liguobao/code/git.virjar.com/leetcode-py-2021 ; /usr/bin/env /usr/bin/python3 /Users/liguobao/.vscode/extensions/ms-python.python-2021.12.1559732655/pythonFiles/lib/python/debugpy/launcher 56935 -- /Users/liguobao/code/git.virjar.com/leetcode-py-2021/daily-code/2000.reversePrefix.py

---
+++ daily-code/2000.reversePrefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        """
+        :type word: str
+        :type ch: str
+        :rtype: str
+        """
+        if ch not in word:
+            return word
+        ch_index = word.index(ch)
+        sub_text = word[0:ch_index+1]
+        rev_text = sub_text[::-1]
+        remove_text = word.replace(sub_text, rev_text)
+        return remove_text
+
+
+word = "abcdefd"
+ch = "d"
+result = Solution().reversePrefix(word, ch)
+print(f"input_text:{word},{ch},result:{result}")
