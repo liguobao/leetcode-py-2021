6bfa19e6db28d1becca080fab279b0dd2e6ea6be
liguobao <codelover@qq.com>
2022-03-10 16:42:09 +0800

    add lru

--- daily-code/146.lur.20220310.py
+++ daily-code/146.lur.20220310.py
@@ -15,10 +15,10 @@ class LRUCache:
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def get(self, key: int):
+    def get(self, key: int) -> int:
         if key in self.hashmap:
             self.move_to_tail(key)
-        c_node: ListNode = self.hashmap.get(key, None)
+        c_node:ListNode = self.hashmap.get(key, None)
         return c_node.val if c_node else -1
 
     def put(self, key: int, value: int) -> None:
@@ -26,42 +26,35 @@ class LRUCache:
             self.move_to_tail(key, value)
             return
         if len(self.hashmap) == self.capacity:
-            first_node = self.head.next
+            first_node: ListNode = self.head.next
+            # 从Hashmap中移除节点
             self.hashmap.pop(first_node.key)
-            self.head.next = first_node.next
+            # 切换掉第一个节点前后数据
             first_node.next.prev = self.head
-        new_node = ListNode(key=key,val=value)
+            self.head.next = first_node.next
+        new_node = ListNode(key=key, val=value)
         self.hashmap[key] = new_node
         last_node = self.tail.prev
         last_node.next = new_node
         new_node.prev = last_node
         self.tail.prev = new_node
         new_node.next = self.tail
-        
-
-    def move_to_tail(self, key, c_val=None):
-        """把节点移到尾部（头部的数据先被淘汰）
-        Args:
-            key ([type]): [description]
-        """
-        move_node:ListNode = self.hashmap[key]
-        # 移除当前节点的前后关联
-        move_node.next.prev = move_node.prev
-        move_node.prev.next = move_node.next
        
-        
-        # 先把最后一个节点拿出来
-        last_node = self.tail.prev
-        # 设置当前最后节点的下一个节点为新节点
-        last_node.next = move_node
-        move_node.prev = last_node
-        
-        # 设置新节点和尾结点的关系
-        self.tail.prev = move_node
+
+    def move_to_tail(self, key, current_val=None):
+        move_node: ListNode = self.hashmap[key]
+        if current_val:
+            move_node.val = current_val
+        # 抽离当前元素
+        prev_node = move_node.prev
+        prev_node.next = move_node.next
+        move_node.next.prev = prev_node
+
+        # 把当前元素放到队尾
+        self.tail.prev.next = move_node
+        move_node.prev = self.tail.prev
         move_node.next = self.tail
-        if c_val:
-            move_node.val = c_val
-        return
+        self.tail.prev = move_node
 
 
 # Your LRUCache object will be instantiated and called as such:
