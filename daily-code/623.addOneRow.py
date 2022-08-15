2fe7321318ea2abaeaf5634a8c5eabf07818d56e
liguobao <codelover@qq.com>
2022-08-16 02:09:24 +0800

    feat: 更新AddOneRow

--- daily-code/623.addOneRow.py
+++ daily-code/623.addOneRow.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+from inspect import stack
 from b_tree import BinaryTree
 
 
@@ -7,7 +8,6 @@ class TreeNode(object):
         self.val = val
         self.left = left
         self.right = right
-
 
 class Solution(object):
     def addOneRow(self, root, val, depth):
@@ -20,8 +20,8 @@ class Solution(object):
         if depth == 1:
             return TreeNode(val, root, None)
         stack_node = [root]
-        # 层次遍历，所有节点按层存储到数据
-        level_array = []
+        # 数组每一层对于同一个深度的节点
+        all_level_nodes = []
         while stack_node:
             stack_len = len(stack_node)
             level_nodes = []
@@ -34,9 +34,10 @@ class Solution(object):
                 if current_node.right:
                     stack_node.append(current_node.right)
                 stack_len = stack_len - 1
-            level_array.append(level_nodes)
-        # 找到需要处理的那一行
-        for node in level_array[depth-2]:
+            all_level_nodes.append(level_nodes)
+        # 给定深度，则操作给定深度的 上一层
+        # 因为深度从1开始算，则给定深度上一层为 depth-2
+        for node in all_level_nodes[depth - 2]:
             node.left = TreeNode(val, node.left, None)
             node.right = TreeNode(val, None, node.right)
         return root
